85e6d098d42755ce245fcaa9abc241a9d0eb3aab
Thomas Schreiner <iteano.thom@hotmail.com>
2022-09-10 13:44:29 -0300

    feat: add logout functionality

--- src/pages/dashboard/dashboard.js
+++ src/pages/dashboard/dashboard.js
@@ -5,10 +5,18 @@ class DashboardLogado {
     static alterarBotoesHeader() {
         const btnHeader = document.querySelectorAll("header li .text-2")
         btnHeader.forEach(btn => btn.classList.toggle("hidden"))
+        
+        DashboardLogado.logout()
     }
 
     static logout() {
-        
+        const btnHeader = document.querySelector("header li button")
+        btnHeader.addEventListener("click", () => {
+            localStorage.removeItem("@kenzieEmpresas:token")
+            localStorage.removeItem("@kenzieEmpresas:is_admin")
+            localStorage.removeItem("@kenzieEmpresas:uuid")
+            window.location.replace("../../../index.html")
+        })
     }
 }
 
@@ -20,7 +28,6 @@ function verifyUser() {
 
     if(token) {
         DashboardLogado.alterarBotoesHeader()
-
         if(isAdmin) {
             // AdminUser
             body.classList.add("body__admin")
